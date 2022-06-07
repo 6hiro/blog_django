98210458f3e62c65eb8e346841e06fd0b9984473
6hiro <hiro6github@gmail.com>
2022-06-07 12:33:34 +0900

    first commit

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('articles/', include('articles.urls', namespace='articles')),
     re_path('markdownx/', include('markdownx.urls')),
 ]
-
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
